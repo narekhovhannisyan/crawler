a77ba6eb9a6c6f94b32c6ef97f6854cf10fee67c
narekhovhannisyan <narekgilmour@gmail.com>
2018-08-08 21:32:02 +0400

    social: in facebook.js get rid of models
     - refactor all functions
     - add JSDocs for all functions.

--- src/social/facebook.js
+++ src/social/facebook.js
@@ -1,37 +1,53 @@
 const env = process.env.NODE_ENV || 'development'
 const config = require(__dirname + '/../config/config.json')[env]
 const rp = require('request-promise')
-const models = require('../db')
 
-
+/**
+ * @param uri - Uri to make request.
+ * @returns {Promise.<>}
+ * @description Makes request to given fb `uri`.
+ */
 const makeFbRequest = (uri) => {
-	uri += '&access_token=' + config.FACEBOOK_APP_ID + '|' + config.FACEBOOK_APP_SECRET
-	let options = {
-		uri: uri,
-		headers: {
-				'User-Agent': 'Request-Promise'
-		},
-		json: true 
-	}
-	return rp(options)
+  const fbUri = uri + '&access_token=' + config.FACEBOOK_APP_ID + '|' + config.FACEBOOK_APP_SECRET
+  const options = {
+    uri: fbUri,
+    headers: {
+      'User-Agent': 'Request-Promise'
+    },
+    json: true
+  }
+
+  return rp(options)
 }
 
+/**
+ * @param postId - The post id.
+ * @param fields - Fields to get.
+ * @returns {Promise.<>}
+ * @description Gets post details by given `postId` and `fields`.
+ */
 const getPostDetails = (postId, fields) => {
-	let fieldeghen = fields ? '?fields=' + fields.join(',') : ''
-	let url = 'https://graph.facebook.com/v2.9/' + postId + fieldeghen
-	return makeFbRequest(url)
+  const fieldsString = fields ? '?fields=' + fields.join(',') : ''
+  const url = 'https://graph.facebook.com/v2.9/' + postId + fieldsString
+
+  return makeFbRequest(url)
 }
 
+/**
+ * @param pageId - The page id.
+ * @param limit - Posts count by page.
+ * @param fields - Fields to get.
+ * @returns {Promise.<>}
+ * @description Gets page posts by given `pageId`.
+ */
 const getPagePosts = (pageId, limit, fields) => {
-	let urlazavr = '?fields=posts.limit=(' + limit + ')' + '{' + fields.join(',') + '}'
-	let url = 'https://graph.facebook.com/v2.9/' + pageId + urlazavr
-	return makeFbRequest(url).then(res => {
-		return res.posts.data.map(post => Object.assign({}, post, {pageId}))
-	})
-}
+  const urlazavr = '?fields=posts.limit=(' + limit + ')' + '{' + fields.join(',') + '}'
+  const url = 'https://graph.facebook.com/v2.9/' + pageId + urlazavr
 
+  return makeFbRequest(url).then(res => res.posts.data.map(post => Object.assign({}, post, {pageId})))
+}
 
 module.exports = {
-	getPostDetails,
-	getPagePosts
+  getPostDetails,
+  getPagePosts
 }
